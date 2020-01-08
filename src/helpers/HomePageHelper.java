816d9010b45c34d7b08d7b02c74282ff86454702
KseniaPermyakova <ksyshenjkateplova@gmail.com>
2020-01-08 19:32:46 +0200

    Project architecture was changed

---
+++ src/helpers/HomePageHelper.java
@@ -0,0 +1,22 @@
+package helpers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePageHelper extends PageBase {
+
+    public HomePageHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public void waitUntilPageIsLoaded() {
+        waitUntilElementIsClickable(By.xpath("//a[@class='btn btn-sm btn-link text-white']"), 40);
+    }
+
+    public void openLoginPage() {
+        WebElement loginIcon = driver.findElement(By.xpath("//a[@class='btn btn-sm btn-link text-white']"));
+        loginIcon.click();
+    }
+
+}
